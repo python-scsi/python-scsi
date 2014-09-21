9805be2e682670328aef8665592bfb7d94371ca8
Markus Rosjat <markus.rosjat@gmail.com>
2014-09-21 15:31:23 +0200

    added some print statements to get nicer output

--- sgio/tests/test_inquiry.py
+++ sgio/tests/test_inquiry.py
@@ -6,14 +6,20 @@ import sys
 from sgio.pyscsi.scsi import SCSI
 from sgio.pyscsi import scsi_cdb_inquiry as INQUIRY
 
+
 def main(device):
     s = SCSI(device)
 
+    print 'inquiry without enabled vital product data'
+    print '==========================================\n'
     i = s.inquiry().result
-    print i
-
+    for k, v in i.iteritems():
+        print '%s - %s' % (k, v)
+    print '\ninquiry with enabled vital product data\nand proper page_code'
+    print '==========================================\n'
     i = s.inquiry(evpd = 1, page_code = INQUIRY.VPD.SUPPORTED_VPD_PAGES).result
-    print i
+    for k, v in i.iteritems():
+        print '%s - %s' % (k, v)
 
 if __name__ == "__main__":
     main(sys.argv[1])
