4764f143b78d26b954c86f2a008fdeadf5f0fb87
mr <markus.rosjat@gmail.com>
2014-09-25 19:40:48 +0200

    add SCSIDeviceExceptionMeta to scsi_exception

--- sgio/pyscsi/scsi_exception.py
+++ sgio/pyscsi/scsi_exception.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 
+from scsi_sense import SCSICheckCondition
 
 class SCSICommandExceptionMeta(type):
     """A meta class for class depending SCSICommand exceptions
 
     """
 
-    def __new__(meta, cls, bases, attributes):
+    def __new__(mcs, cls, bases, attributes):
 
         class CommandNotImplemented(Exception):
             pass
@@ -17,4 +18,23 @@ class SCSICommandExceptionMeta(type):
         attributes.update({'CommandNotImplemented': CommandNotImplemented})
         attributes.update({'OpcodeException': OpcodeException})
 
-        return type.__new__(meta, cls, bases, attributes)
+        return type.__new__(mcs, cls, bases, attributes)
+
+
+class SCSIDeviceExceptionMeta(type):
+    """A meta class for class depending SCSICommand exceptions
+
+    """
+
+    def __new__(mcs, cls, bases, attributes):
+
+        class CheckCondition(SCSICheckCondition):
+            pass
+
+        class SCSISGIOError(Exception):
+            pass
+
+        attributes.update({'CheckCondition': CheckCondition})
+        attributes.update({'SCSISGIOError': SCSISGIOError})
+
+        return type.__new__(mcs, cls, bases, attributes)
