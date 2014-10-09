a2e02a9301d9243bc59c4633bcf9090d6ad7e8bb
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2014-10-09 06:25:35 -0700

    tests: fix readcapacity16 unmarshalling and add unit test
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- sgio/tests/__init__.py
+++ sgio/tests/__init__.py
@@ -1,3 +1,4 @@
 __all__ = ['test_inquiry', 'test_enum',
            'test_unmarshall_inquiry', 
-           'test_unmarshall_readcapacity10', ]
+           'test_unmarshall_readcapacity10',
+           'test_unmarshall_readcapacity16', ]
