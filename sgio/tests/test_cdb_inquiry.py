169fef48222629f4aef51b9c74140ce7aa8076f1
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2014-10-09 19:21:50 -0700

    tests: unit tests for marshalling CDBs for the commands we support
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

---
+++ sgio/tests/test_cdb_inquiry.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+from sgio.pyscsi.scsi import SCSI
+from sgio.utils.converter import scsi_ba_to_int
+from sgio.pyscsi.scsi_command import OPCODE
+
+class MockInquiry(object):
+   def execute(self, cdb, dataout, datain, sense):
+      pass
+
+def main():
+    s = SCSI(MockInquiry())
+
+    # cdb for standard page request
+    cdb = s.inquiry(alloc_len=128)._cdb
+    assert cdb[0] == OPCODE.INQUIRY
+    assert cdb[1:3] == bytearray(2)
+    assert scsi_ba_to_int(cdb[3:5]) == 128
+    assert cdb[5] == 0
+
+    # supported vpd pages
+    cdb = s.inquiry(evpd=1, page_code=0x88, alloc_len=128)._cdb
+    assert cdb[0] == OPCODE.INQUIRY
+    assert cdb[1] == 0x01
+    assert cdb[2] == 0x88
+    assert scsi_ba_to_int(cdb[3:5]) == 128
+    assert cdb[5] == 0
+
+if __name__ == "__main__":
+    main()
+
---
+++ sgio/tests/test_cdb_readcapacity16.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+from sgio.pyscsi.scsi import SCSI
+from sgio.utils.converter import scsi_ba_to_int
+from sgio.pyscsi.scsi_command import OPCODE, SERVICE_ACTION_IN
+
+class MockReadCapacity16(object):
+   def execute(self, cdb, dataout, datain, sense):
+      pass
+
+def main():
+    s = SCSI(MockReadCapacity16())
+
+    cdb = s.readcapacity16(37)._cdb
+    assert cdb[0] == OPCODE.SERVICE_ACTION_IN
+    assert cdb[1] == SERVICE_ACTION_IN.READ_CAPACITY_16
+    assert cdb[2:10] == bytearray(8)
+    assert scsi_ba_to_int(cdb[10:14]) == 37
+    assert cdb[14:16] == bytearray(2)
+
+if __name__ == "__main__":
+    main()
+
