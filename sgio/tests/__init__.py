d66e0e55cd252bd9a9968589c181b164d9b0f29d
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2014-10-19 09:57:10 -0700

    Add initial support for MODESENSE6
    Add initial support for MODESENSE6
    Add support for one page, SMC Element Address Assignment
    
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- sgio/tests/__init__.py
+++ sgio/tests/__init__.py
@@ -1,5 +1,6 @@
 __all__ = ['test_inquiry', 'test_enum',
            'test_cdb_inquiry',
+           'test_cdb_modesense6',
            'test_cdb_read10',
            'test_cdb_read12',
            'test_cdb_read16',
@@ -9,5 +10,6 @@ __all__ = ['test_inquiry', 'test_enum',
            'test_cdb_write12',
            'test_cdb_write16',
            'test_unmarshall_inquiry', 
+           'test_unmarshall_modesense6', 
            'test_unmarshall_readcapacity10',
            'test_unmarshall_readcapacity16', ]
