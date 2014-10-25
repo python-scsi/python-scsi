e4aaf48962b2701f37b0b5f77565a01ae77815c6
mr <markus.rosjat@gmail.com>
2014-10-25 19:10:45 +0200

    just some pep8 formating in test_unmarshall_inquiry

--- sgio/tests/test_unmarshall_inquiry.py
+++ sgio/tests/test_unmarshall_inquiry.py
@@ -8,14 +8,14 @@ from sgio.pyscsi import scsi_cdb_inquiry as INQUIRY
 
 class MockInquiryStandard(object):
    def execute(self, cdb, dataout, datain, sense):
-       datain[0] = 0x25 # QUAL:1 TYPE:5
-       datain[1] = 0x80 # RMB:1
-       datain[2] = 0x07 # VERSION:7
-       datain[3] = 0x23 # NORMACA:1 HISUP:0 RDF:3
-       datain[4] = 0x40 # ADDITIONAL LENGTH:64
-       datain[5] = 0xb9 # SCCS:1 ACC:0 TGPS:3 3PC:1 PROTECT:1
-       datain[6] = 0x71 # ENCSERV:1 VS:1 MULTIP:1 ADDR16:1
-       datain[7] = 0x33 # WBUS16:1 SYNC:1 CMDQUE:1 VS2:1
+       datain[0] = 0x25  # QUAL:1 TYPE:5
+       datain[1] = 0x80  # RMB:1
+       datain[2] = 0x07  # VERSION:7
+       datain[3] = 0x23  # NORMACA:1 HISUP:0 RDF:3
+       datain[4] = 0x40  # ADDITIONAL LENGTH:64
+       datain[5] = 0xb9  # SCCS:1 ACC:0 TGPS:3 3PC:1 PROTECT:1
+       datain[6] = 0x71  # ENCSERV:1 VS:1 MULTIP:1 ADDR16:1
+       datain[7] = 0x33  # WBUS16:1 SYNC:1 CMDQUE:1 VS2:1
        # t10 vendor id
        datain[8:16] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
        # product id
@@ -23,25 +23,25 @@ class MockInquiryStandard(object):
                         'j', 'j','j', 'j','j', 'j','j', 'j']
        # product revision level
        datain[32:36] = ['r', 'e', 'v', 'n']
-       datain[56] = 0x09 # CLOCKING:2 QAS:0 IUS:1
+       datain[56] = 0x09  # CLOCKING:2 QAS:0 IUS:1
 
 class MockLBP(object):
    def execute(self, cdb, dataout, datain, sense):
-       datain[0] = 0x00 # QUAL:0 TYPE:0
-       datain[1] = 0xb2 # logical block provisioning
-       datain[2] = 0x00 #
-       datain[3] = 0x04 # page length == 4
-       datain[4] = 0x12 # threshold exponent
-       datain[5] = 0xe7 # LBPU:1 LBPWS:1 LBPWS10:1 LBPRZ:1 ANC_SUP:1 DP:1
-       datain[6] = 0x02 # Provisioning Type:2
-       datain[7] = 0x00 # 
+       datain[0] = 0x00  # QUAL:0 TYPE:0
+       datain[1] = 0xb2  # logical block provisioning
+       datain[2] = 0x00  #
+       datain[3] = 0x04  # page length == 4
+       datain[4] = 0x12  # threshold exponent
+       datain[5] = 0xe7  # LBPU:1 LBPWS:1 LBPWS10:1 LBPRZ:1 ANC_SUP:1 DP:1
+       datain[6] = 0x02  # Provisioning Type:2
+       datain[7] = 0x00  #
 
 class MockUSN(object):
    def execute(self, cdb, dataout, datain, sense):
-       datain[0] = 0x00 # QUAL:0 TYPE:0
-       datain[1] = 0xb2 # logical block provisioning
-       datain[2] = 0x00 #
-       datain[3] = 0x04 # page length == 4
+       datain[0] = 0x00  # QUAL:0 TYPE:0
+       datain[1] = 0xb2  # logical block provisioning
+       datain[2] = 0x00  #
+       datain[3] = 0x04  # page length == 4
        datain[4:8] = "ABCD"
 
 def main():
