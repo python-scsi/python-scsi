ef94d0be764ea64136acb4574b24e8d3011aaf57
rosjat <markus.rosjat@gmail.com>
2014-12-30 23:30:15 +0100

    fixed broken scsi_device modul
     with original code from sahlberg/setup branch.

--- sgio/pyscsi/scsi_device.py
+++ sgio/pyscsi/scsi_device.py
@@ -21,39 +21,71 @@ import sgio
 
 from scsi_exception import SCSIDeviceCommandExceptionMeta as ExMETA
 
+try:
+    import libiscsi
+    _have_libiscsi = True
+except:
+    _have_libiscsi = False
+
 
 class SCSIDevice(object):
     """
-    The base class for a derived scsi device class.
-
+    The base class for a derived  scsi device class
     """
     __metaclass__ = ExMETA
 
-    def __init__(self, device, readwrite=False):
+    def __init__(self, device):
         """
-        Open and initialize a new sg device instance.
-
-        :param device:    The pathname of the device to open.
-
-        :param readwrite: If False (default) the device will be opened in
-                          read-only mode, otherwise it will be opened in
-                          read/write mode.
+        initialize a  new instance
+        :param device: the file descriptor
         """
-        readwrite = bool(readwrite)
-        self._fd = sgio.open(device, readwrite)
+        if _have_libiscsi and device[:8] == 'iscsi://':
+            self._iscsi = libiscsi.iscsi_create_context('iqn.2007-10.com.github:python-scsi')
+            self._iscsi_url = libiscsi.iscsi_parse_full_url(self._iscsi, device)
+            libiscsi.iscsi_set_targetname(self._iscsi, self._iscsi_url.target)
+            libiscsi.iscsi_set_session_type(self._iscsi, libiscsi.ISCSI_SESSION_NORMAL)
+            libiscsi.iscsi_set_header_digest(self._iscsi, libiscsi.ISCSI_HEADER_DIGEST_NONE_CRC32C)
+            libiscsi.iscsi_full_connect_sync(self._iscsi, self._iscsi_url.portal, self._iscsi_url.lun)
+
+            self._is_libiscsi = True
+        else:
+            self._fd = sgio.open(device)
 
     def execute(self, cdb, dataout, datain, sense):
         """
         execute a scsi command
-
         :param cdb: a byte array representing a command descriptor block
         :param dataout: a byte array to hold received data from the ioctl call
         :param datain: a byte array to hold data passed to the ioctl call
         :param sense: a byte array to hold sense data
         """
-        status = sgio.execute(self._fd, cdb, dataout, datain, sense)
-        if status == scsi_enum_command.SCSI_STATUS.CHECK_CONDITION:
-            raise self.CheckCondition(sense)
-        if status == scsi_enum_command.SCSI_STATUS.SGIO_ERROR:
+        if self._is_libiscsi:
+            _dir = libiscsi.SCSI_XFER_NONE
+            _xferlen = 0
+            if len(datain):
+                _dir = libiscsi.SCSI_XFER_READ
+                _xferlen = len(datain)
+            if len(dataout):
+                _dir = libiscsi.SCSI_XFER_WRITE
+                _xferlen = len(dataout)
+            _task = libiscsi.scsi_create_task(cdb, _dir, _xferlen)
+            if len(datain):
+                libiscsi.scsi_task_add_data_in_buffer(_task, datain)
+            if len(dataout):
+                libiscsi.scsi_task_add_data_out_buffer(_task, dataout)
+
+            libiscsi.iscsi_scsi_command_sync(self._iscsi, self._iscsi_url.lun, _task, None)
+            _status = libiscsi.scsi_task_get_status(_task, None)
+            if _status == libiscsi.SCSI_STATUS_CHECK_CONDITION:
+                raise self.CheckCondition(sense)
+            if _status == libiscsi.SCSI_STATUS_GOOD:
+                return
+
             raise self.SCSISGIOError
 
+        else:
+            status = sgio.execute(self._fd, cdb, dataout, datain, sense)
+            if status == scsi_enum_command.SCSI_STATUS.CHECK_CONDITION:
+                raise self.CheckCondition(sense)
+            if status == scsi_enum_command.SCSI_STATUS.SGIO_ERROR:
+                raise self.SCSISGIOError
