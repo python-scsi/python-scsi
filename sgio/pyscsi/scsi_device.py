9d7380366d6776c44deff8318b3df23758ce978e
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2014-12-30 20:56:02 -0800

    scsi_device.py: use linux_sgio module instead of the sgio module
    We are going to remove the builtin sgio module and instead use an external,
    conditionally imported, module called linux_sgio.
    Name is meant to indicate that this is a linux specific module.
    
    This linux_sgio module is linux specific and will not be available on
    non-linux systems which is why we import it conditionally but will not
    fail if it is not available.
    This is to allow using python-scsi on other, non-linux, systems.
    The aim is to allow to create windows/solaris/ios/... specific modules for
    accessing scsi devices and plugging them into python-scsi just like the
    existing libiscsi module we already have and the linux_sgio module we add in
    this patch.
    
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- sgio/pyscsi/scsi_device.py
+++ sgio/pyscsi/scsi_device.py
@@ -17,7 +17,6 @@
 #	   along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import scsi_enum_command
-import sgio
 
 from scsi_exception import SCSIDeviceCommandExceptionMeta as ExMETA
 
@@ -27,13 +26,20 @@ try:
 except:
     _have_libiscsi = False
 
+try:
+    import linux_sgio
+    _have_linux_sgio = True
+except:
+    _have_linux_sgio = False
+
+
 class SCSIDevice(object):
     """
     The base class for a derived  scsi device class
     """
     __metaclass__ = ExMETA
 
-    def __init__(self, device):
+    def __init__(self, device, readwrite=False):
         """
         initialize a  new instance
         :param device: the file descriptor
@@ -47,8 +53,9 @@ class SCSIDevice(object):
             libiscsi.iscsi_full_connect_sync(self._iscsi, self._iscsi_url.portal, self._iscsi_url.lun)
 
             self._is_libiscsi = True
-        else:
-            self._fd = sgio.open(device)
+        elif _have_linux_sgio and device[:7] == '/dev/sg':
+            self._is_linux_sgio = True
+            self._fd = linux_sgio.open(device, bool(readwrite))
 
     def execute(self, cdb, dataout, datain, sense):
         """
@@ -82,8 +89,8 @@ class SCSIDevice(object):
 
             raise self.SCSISGIOError
 
-        else:
-            status = sgio.execute(self._fd, cdb, dataout, datain, sense)
+        elif hasattr(self, '_is_linux_sgio'):
+            status = linux_sgio.execute(self._fd, cdb, dataout, datain, sense)
             if status == scsi_enum_command.SCSI_STATUS.CHECK_CONDITION:
                 raise self.CheckCondition(sense)
             if status == scsi_enum_command.SCSI_STATUS.SGIO_ERROR:
