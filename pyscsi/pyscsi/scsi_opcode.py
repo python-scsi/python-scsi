aba677b965d805538b37101971d64d4ad832cb43
rosjat <markus.rosjat@gmail.com>
2015-01-11 14:58:08 +0100

    add OpCode class to scsi_opcode
    first version of  the OpCode class just to show
    the intention behind it. A dict in the scsi_enum_command
    would look like  this:
    
     spc_opcodes = { 'REPORT_ALIASES': Opcode(
     'REPORT_ALLIASES', 0xa3, {'REPORT_ALIASES': 0x0b, }),   }

--- pyscsi/pyscsi/scsi_opcode.py
+++ pyscsi/pyscsi/scsi_opcode.py
@@ -1,31 +1,75 @@
 # coding: utf-8
 
+# Copyright (C) 2015 by Markus Rosjat<markus.rosjat@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 from pyscsi.utils.enum import Enum
 
 
-class OpCodeMapper(object):
+class OpCode(object):
 
-    def __init__(self, opcodes, serviceaction):
-        self._opcode = Enum(opcodes)
+    _name = ''
+    _code = 0xff
+    _serviceaction = None
+
+    def __init__(self, name, code, serviceaction):
+        self._name = name
+        self._code = code
         self._serviceaction = Enum(serviceaction)
 
+    def __str__(self):
+        return '%s - %x' % (self.name, self.value)
+
+    def __repr__(self):
+        return '%s - %x' % (self.name, self.value)
+
     @property
-    def opcode(self):
+    def name(self):
         """
-        getter method of the opcode property
+        getter method of the name property
 
-        :return: a Enum object
+        :return: a  string
         """
-        return self._opcode
+        return self._name
 
-    @opcode.setter
-    def opcode(self, value):
+    @name.setter
+    def name(self, value):
         """
-        setter method of the result property
+        setter method of the name property
 
-        :param value: a Enum object
+        :param value: a string
         """
-        self._opcode = value
+        self._name = value
+
+    @property
+    def value(self):
+        """
+        getter method of the value property
+
+        :return: a hex value
+        """
+        return self._code
+
+    @value.setter
+    def value(self, value):
+        """
+        setter method of the value property
+
+        :param value: a hex value
+        """
+        self._code = value
 
     @property
     def serviceaction(self):
@@ -44,3 +88,27 @@ class OpCodeMapper(object):
         :param value: a Enum object
         """
         self._serviceaction = value
+
+
+class OpCodeMapper(object):
+
+    def __init__(self, opcodes):
+        self._opcode = Enum(opcodes)
+
+    @property
+    def opcode(self):
+        """
+        getter method of the opcode property
+
+        :return: a Enum object
+        """
+        return self._opcode
+
+    @opcode.setter
+    def opcode(self, value):
+        """
+        setter method of the opcode property
+
+        :param value: a Enum object
+        """
+        self._opcode = value
