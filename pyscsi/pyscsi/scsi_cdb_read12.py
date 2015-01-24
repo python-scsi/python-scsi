6ba8421e270892d59b9ec3e3362b3c119d9e0960
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2015-01-24 15:15:55 -0800

    read10/12/16: make cdb marshalling/unmarshalling dict driven
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- pyscsi/pyscsi/scsi_cdb_read12.py
+++ pyscsi/pyscsi/scsi_cdb_read12.py
@@ -17,7 +17,7 @@
 
 from scsi_command import SCSICommand
 from scsi_enum_command import OPCODE
-from pyscsi.utils.converter import scsi_int_to_ba, decode_bits
+from pyscsi.utils.converter import scsi_int_to_ba, encode_dict, decode_bits
 
 #
 # SCSI Read12 command and definitions
@@ -28,6 +28,15 @@ class Read12(SCSICommand):
     """
     A class to send a Read(12) command to a scsi device
     """
+    _cdb_bits = {'opcode': [0xff, 0],
+                 'rdprotect': [0xe0, 1],
+                 'dpo': [0x10, 1],
+                 'fua': [0x08, 1],
+                 'rarc': [0x04, 1],
+                 'lba': [0xffffffff, 2],
+                 'tl': [0xffffffff, 6],
+                 'group': [0x1f, 10]
+    }
 
     def __init__(self, scsi, lba, tl, **kwargs):
         SCSICommand.__init__(self, scsi, 0, scsi.blocksize * tl)
@@ -38,29 +47,32 @@ class Read12(SCSICommand):
         """
         Build a Read12 CDB
         """
-        cdb = self.init_cdb(self.scsi.device.opcodes.READ_12.value)
-        cdb[2:6] = scsi_int_to_ba(lba, 4)
-        cdb[6:10] = scsi_int_to_ba(tl, 4)
-        cdb[1] |= (rdprotect << 5) & 0xe0
-        cdb[1] |= 0x10 if dpo else 0
-        cdb[1] |= 0x08 if fua else 0
-        cdb[1] |= 0x04 if rarc else 0
-        cdb[10] |= group & 0x1f
+        cdb = {
+            'opcode': self.scsi.device.opcodes.READ_12.value,
+            'lba': lba,
+            'tl': tl,
+            'rdprotect': rdprotect,
+            'dpo': dpo,
+            'fua': fua,
+            'rarc': rarc,
+            'group': group,
+        }
+        return self.marshall_cdb(cdb)
 
-        return cdb
+    @staticmethod
+    def unmarshall_cdb(cdb):
+        """
+        Unmarshall a Read12 cdb
+        """
+        result = {}
+        decode_bits(cdb, Read12._cdb_bits, result)
+        return result
 
-    def unmarshall_cdb(self, cdb):
+    @staticmethod
+    def marshall_cdb(cdb):
         """
-        method to unmarshall a byte array containing a cdb.
+        Marshall a Read12 cdb
         """
-        _tmp = {}
-        _bits = {'opcode': [0xff, 0],
-                 'rdprotect': [0xe0, 1],
-                 'dpo': [0x10, 1],
-                 'fua': [0x08, 1],
-                 'rarc': [0x04, 1],
-                 'lba': [0xffffffff, 2],
-                 'tl': [0xffffffff, 6],
-                 'group': [0x1f, 10], }
-        decode_bits(cdb, _bits, _tmp)
-        return _tmp
+        result = bytearray(12)
+        encode_dict(cdb, Read12._cdb_bits, result)
+        return result
