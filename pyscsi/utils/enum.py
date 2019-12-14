c4e2687f507a155942d99ca66090eede265c3b5a
rosjat <markus.rosjat@gmail.com>
2019-12-14 14:56:25 +0100

    refactor Enum to just work with a dict or keyword arguments
    simply assuming we pass a dict or a dict and kwargs or just kwargs to Enum.
    It is still wonky but it works for this. If this moves to a python 3 only
    code base we should consider the builtin enum type of python.
    
    Signed-off-by: rosjat <markus.rosjat@gmail.com>

--- pyscsi/utils/enum.py
+++ pyscsi/utils/enum.py
@@ -32,57 +32,37 @@ class Enum(type):
 
         for now there is not much of a sanity check here, like if the key already exists or if a enum
         value is already there. It's basically a little helper for the other packages in this project
-        and for now the developer has to take care that stuff is is sane.
+        and for now the developer has to take care that stuff is is sane. And we assume  that we pass either
+        a dict or simply keyword arguments!
 
         TODO:
 
           - adding check if value exists
     """
 
-    def __new__(cls,
-                *args,
-                **kwargs):
-        if len(args) > 0:
-            if len(args) == 1:
-                if type(args[0]).__name__ == 'dict':
-                    tmp = args[0]
-                else:
-                    tmp = {}
-            else:
-                tmp = dict()
-        elif len(kwargs) > 0:
-            if len(kwargs) == 1:
-                tmp = kwargs
-            else:
-                tmp = dict(**kwargs)
+    def __new__(cls, *args, **kwargs):
+        """
+        Building a new Enum object with a dict or keyword arguments
+        """
+        tmp = {}
+        if len(args) == 1 and type(args[0]).__name__ == 'dict':
+            tmp.update(args[0])
         else:
-            tmp = dict()
+            tmp.update(dict(**kwargs))
         tmp['_enums'] = tmp.keys()
-        return type.__new__(cls,
-                            cls.__name__,
-                            (),
-                            tmp)
+        return type.__new__(cls, cls.__name__, (), tmp)
 
-    def __init__(self,
-                 *args,
-                 **kwargs):
-        setattr(self,
-                'add',
-                classmethod(self.__class__.add))
-        setattr(self,
-                'remove',
-                classmethod(self.__class__.remove))
+    def __init__(self, *args, **kwargs):
+        setattr(self, 'add', classmethod(self.__class__.add))
+        setattr(self, 'remove', classmethod(self.__class__.remove))
 
-    def __getitem__(self,
-                    value):
+    def __getitem__(self, value):
         for key in self._enums:
-                if self.__getattribute__(self, key) == value:
-                    return key
+            if self.__getattribute__(self, key) == value:
+                return key
         return ""
 
-    def add(self,
-            key,
-            value):
+    def add(self, key, value):
         try:
             getattr(self,
                     key)
@@ -93,11 +73,9 @@ class Enum(type):
                     key,
                     value)
 
-    def remove(self,
-               key):
+    def remove(self, key):
         try:
-            delattr(self,
-                    key)
+            delattr(self, key)
             self._enums.remove(key)
         except (AttributeError, KeyError) as e:
-            print ("Key %s not found" % e)
+            print("Key %s not found" % e)
--- tests/test_enum.py
+++ tests/test_enum.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 from pyscsi.utils.enum import Enum
+from pyscsi.pyscsi.scsi_enum_command import smc
 
 enum_dict = {'A': 1,
              'B': 2,
@@ -17,6 +18,16 @@ def main():
     assert i[2] == 'B'
     assert i[3] == 'C'
     assert i[4] == ''
+    a = Enum(A=1, B=2, C=3)
+    assert a.A == 1
+    assert a.B == 2
+    assert a.C == 3
+    assert a[1] == 'A'
+    assert a[2] == 'B'
+    assert a[3] == 'C'
+    assert a[4] == ''
+    assert smc.WRITE_BUFFER.value == 0x3b
+    assert smc.WRITE_BUFFER.name == 'WRITE_BUFFER'
 
 
 if __name__ == "__main__":
