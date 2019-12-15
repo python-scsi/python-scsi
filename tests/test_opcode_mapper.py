d2e7a86cd828b059457adcbc78e8102ca79e1081
Markus Rosjat <markus.rosjat@gmail.com>
2019-12-15 12:57:41 +0100

    removed unnecessary variable assignments
    Signed-off-by: rosjat <markus.rosjat@gmail.com>

--- tests/test_opcode_mapper.py
+++ tests/test_opcode_mapper.py
@@ -5,16 +5,12 @@ from pyscsi.pyscsi.scsi_enum_command import spc, sbc, ssc, smc
 
 
 def main():
-    _spc = spc
-    _sbc = sbc
-    _ssc = ssc
-    _smc = smc
+    assert spc.SPC_OPCODE_A4.value == 0xa4
+    assert sbc.SBC_OPCODE_9E.value == 0x9e
+    assert ssc.READ_ELEMENT_STATUS_ATTACHED.value == 0xb4
+    assert smc.MAINTENANCE_IN.value == 0xa3
+    assert smc.MAINTENANCE_IN.serviceaction.REPORT_DEVICE_IDENTIFICATION == 0x07
 
-    assert _spc.SPC_OPCODE_A4.value == 0xa4
-    assert _sbc.SBC_OPCODE_9E.value == 0x9e
-    assert _ssc.READ_ELEMENT_STATUS_ATTACHED.value == 0xb4
-    assert _smc.MAINTENANCE_IN.value == 0xa3
-    assert _smc.MAINTENANCE_IN.serviceaction.REPORT_DEVICE_IDENTIFICATION == 0x07
-    
+
 if __name__ == "__main__":
     main()
