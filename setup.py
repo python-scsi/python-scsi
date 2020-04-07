7e0f984b6fd2b1f9f15050429efc315e858fee57
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-04-07 12:46:31 +0100

    Move static setup parameters to `setup.cfg`.
    This allows separating the dynamic information (and the dependencies) from
    the package description.
    
    In addition to moving them:
    
     - Include the URL pointing at the GitHub repository.
     - Replace Ronnie's contact with Markus's (as maintainer).
     - Add LICENSE to the packaging.
     - Add long_description, keywords and classifiers.

--- setup.py
+++ setup.py
@@ -3,13 +3,7 @@
 from setuptools import find_packages, setup
 
 # lets prepare our initial setup
-setup_dict = {'name': 'PYSCSI',
-              'version': '1.0',
-              'license': 'LGPLv2.1',
-              'author': 'Ronnie Sahlberg',
-              'author_email': 'ronniesahlberg@gmail.com',
-              'description': 'Module for calling SCSI devices from Python',
-              'packages': find_packages(),
+setup_dict = {'packages': find_packages(),
               'python_requires': '~=3.7',
               'extras_require': {'sgio': ['cython-sgio'],
                                  'iscsi': ['cython-iscsi'],
