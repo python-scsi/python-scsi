04036b46c9db6b309fc809efae4e8c02114f7847
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-04-07 12:55:52 +0100

    Simplify `setup.py` now that there's no logic and no static info.
    The `setup()` call is no longer conditional on the platform, since the
    platform-dependent modules are "extras" that depend on separate libraries.
    
    And since most of the static settings are now in the `setup.cfg` file, all
    of that is not required even.

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@
 
 from setuptools import find_packages, setup
 
-# lets prepare our initial setup
-setup_dict = {'packages': find_packages(),
-              'python_requires': '~=3.7',
-              'extras_require': {'sgio': ['cython-sgio'],
-                                 'iscsi': ['cython-iscsi'],
-                                 },
-              }
-
-setup(**setup_dict)
+setup(
+    packages=find_packags(),
+    python_requires='~=3.7',
+    extras_require={
+        'sgio': ['cython-sgio'],
+        'iscsi': ['cython-iscsi'],
+    },
+)
