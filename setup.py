346aecf1b2edc9f60679a53c34774574e9e85b52
Markus Rosjat <rosjat@users.noreply.github.com>
2020-04-08 12:03:08 +0200

    fixing typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    packages=find_packags(),
+    packages=find_packages(),
     python_requires='~=3.7',
     extras_require={
         'sgio': ['cython-sgio'],
