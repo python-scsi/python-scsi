850d0851495b83d4669858b0c7f757a1fffa2994
rosjat <markus.rosjat@gmail.com>
2023-01-14 12:37:12 +0100

    adding an exeption module
    so we can start making a few exceptions that might
    that are more suited to the sciope of scsi and iscsi

---
+++ pyscsi/utils/exception.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# Copyright (C) 2022 by Markus Rosjat<markus.rosjat@gmail.com>
+# SPDX-FileCopyrightText: 2014 The python-scsi Authors
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+
+class NotSupportedArgumentError(Exception):
+    ...
